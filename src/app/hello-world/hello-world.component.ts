b458dc8b67ed4f8949a5282c660d76422ef3f46d
Ceuleers Grégory <gregory.ceuleers@bstorm.be>
2020-10-01 15:24:22 +0200

    Directives + bindings

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloWorldComponent implements OnInit {
 
-  constructor() { }
+  variable = 'Affichage de ma donnée depuis le .TS';
+
+  champ: string;
+
+  color: string;
+
+  incrementation = 0;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  affichage(): string {
+    return 'Affiche-moi !!!';
+  }
+
+  afficheDiv(div: HTMLDivElement): string {
+    return div.innerHTML;
+  }
+
+  incr(): void {
+    this.incrementation++;
+  }
+
+  changeValue(select: HTMLSelectElement): void {
+    console.log(select.value);
+  }
 }
